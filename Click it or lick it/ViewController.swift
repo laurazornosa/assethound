50ff533312b5cecd96fd336cee78d2a41de49eaf
laurnosa <laurazornosa@yahoo.com>
2017-04-08 12:40:08 -0500

    first commit

--- Click it or lick it/ViewController.swift
+++ Click it or lick it/ViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer : AVAudioPlayer!
 
+    @IBAction func click(_ sender: Any) {
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let pathToAlertSound = Bundle.main.path(forResource: "clicker", ofType: "mp3")
+        let alertSound = URL(fileURLWithPath: pathToAlertSound!)
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: alertSound)
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
